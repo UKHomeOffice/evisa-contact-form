ebeb9281bf425aa4002a28d979e7c61b06e2cc25
michaelhartHO <144438643+michaelhartHO@users.noreply.github.com>
2024-08-07 15:34:30 +0100

    This linter is configured for incompatibility

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-var, vars-on-top, max-len */
-"use strict";
+'use strict';
 
-require("hof/frontend/themes/gov-uk/client-js");
+require('hof/frontend/themes/gov-uk/client-js');
 
-const accessibleAutocomplete = require("accessible-autocomplete");
+const accessibleAutocomplete = require('accessible-autocomplete');
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Apply the "accessible-autocomplete" plugin to all (form) elements tagged with the class "typeahead"
-  document.querySelectorAll(".typeahead").forEach(function applyTypeahead(element) {
+  document.querySelectorAll('.typeahead').forEach(function applyTypeahead(element) {
     accessibleAutocomplete.enhanceSelectElement({
-      defaultValue: "",
-      selectElement: element,
+      defaultValue: '',
+      selectElement: element
     });
   });
 });
