df4cefc7ce0fed236cbe52d21761f82088c7f9ab
michaelhartHO <144438643+michaelhartHO@users.noreply.github.com>
2024-09-02 11:27:36 +0100

    learn to count

--- test/_unit/custom-validations.spec.js
+++ test/_unit/custom-validations.spec.js
@@ -3,7 +3,7 @@ const { URNValidator } = require('../../apps/evisa/fields/index.js');
 describe('Custom Validators', () => {
   describe('URNValidator', () => {
     it('Returns true for valid URN with - or / separators between 16 and 22 chars in length', () => {
-      expect(URNValidator('1111-2222-3333-4444')).to.equal(true); // 20 characters
+      expect(URNValidator('1111-2222-3333-4444')).to.equal(true); // 19 characters
       expect(URNValidator('1111/2222/3333/4444')).to.equal(true);
     });
     
@@ -12,7 +12,7 @@ describe('Custom Validators', () => {
       expect(URNValidator('1234567890123456')).to.equal(false);
       expect(URNValidator('111-222-333-444')).to.equal(false);
       expect(URNValidator('1111-2222-3333')).to.equal(false); // 14 characters
-      expect(URNValidator('1111/2222/3333/444')).to.equal(false); // 19 characters
+      expect(URNValidator('1111/2222/3333/444')).to.equal(false); // 18 characters
       expect(URNValidator('1111/2222/3333/4444/5555')).to.equal(false); // 24 characters
       expect(URNValidator('123456789012345')).to.equal(false);
       expect(URNValidator('123456789012345X')).to.equal(false);
