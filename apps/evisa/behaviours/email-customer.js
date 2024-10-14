ef62fe38a560460f65d8782109256c89b8a5dcf5
michaelhartHO <144438643+michaelhartHO@users.noreply.github.com>
2024-10-14 10:34:26 +0100

    whitespace

--- apps/evisa/behaviours/email-customer.js
+++ apps/evisa/behaviours/email-customer.js
@@ -7,35 +7,35 @@ const { SESSION } = require('../constants.js');
 const logger = require('hof/lib/logger')({ env: process.env });
 
 module.exports = emailConfig => superclass => class EmailCustomer extends superclass {
-    constructor(...args) {
-      super(...args);
-      this.emailer = new Emailer(emailConfig);
-    }
+  constructor(...args) {
+    super(...args);
+    this.emailer = new Emailer(emailConfig);
+  }
 
-    async successHandler(req, res, next) {
-      if (req.body['continue-button']) {
-        let emailAddress = req.sessionModel.get(SESSION.EMAIL_FIELD);
-        emailAddress = '';
-        if (!emailAddress) {
-          let errorMsg = 'Customer email address missing, unable to send confirmation email';
-          logger.error(errorMsg);
-        } else {
-          let personalisation = this.getCustomerPersonalisation(req.sessionModel);
-          try {
-            await this.emailer.sendCustomerEmail(emailAddress, personalisation);
-          } catch (error) {
-            return next(new Error(req.translate('errors.email.confirmation')));
-          }
+  async successHandler(req, res, next) {
+    if (req.body['continue-button']) {
+      let emailAddress = req.sessionModel.get(SESSION.EMAIL_FIELD);
+      emailAddress = '';
+      if (!emailAddress) {
+        let errorMsg = 'Customer email address missing, unable to send confirmation email';
+        logger.error(errorMsg);
+      } else {
+        let personalisation = this.getCustomerPersonalisation(req.sessionModel);
+        try {
+          await this.emailer.sendCustomerEmail(emailAddress, personalisation);
+        } catch (error) {
+          return next(new Error(req.translate('errors.email.confirmation')));
         }
       }
-      return super.successHandler(req, res, next);
     }
+    return super.successHandler(req, res, next);
+  }
 
-    getCustomerPersonalisation(session) {
+  getCustomerPersonalisation(session) {
     const notSupplied = 'not supplied';
-      return {
+    return {
       'customer-name': session.get(SESSION.FULL_NAME) || notSupplied,
       'customer-question': session.get(SESSION.QUESTION_FIELD) || notSupplied
-      };
-    }
-  };
+    };
+  }
+};
