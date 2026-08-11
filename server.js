01ae8c49ddabd89d5474176b29f8f421ea345998
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-08-11 10:36:24 +0100

    EVC-174 pass upload config to templates for client side validation

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const app = hof(settings);
 app.use((req, res, next) => {
   res.locals.htmlLang = 'en';
   res.locals.feedbackUrl = config.feedbackUrl;
+  res.locals.upload = {
+    allowedMimeTypes: fileUploadConfig.allowedMimeTypes.join(','),
+    maxFileSizeInBytes: String(fileUploadConfig.maxFileSizeInBytes)
+  };
   if (req.is('multipart/form-data')) {
     try {
       const bb = busboy({
